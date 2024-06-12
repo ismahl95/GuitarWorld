9f326c2914b9d588dd735734e091249f08e39262
Ismael <ismael.h.lamela@gmail.com>
2024-06-12 10:05:18 +0200

    chore: Initial commit

---
+++ src/main/java/com/ihl95/GuitarWorld/GuitarWorldApplication.java
@@ -0,0 +1,14 @@
+package com.ihl95.GuitarWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarWorldApplication.class, args);
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ src/test/java/com/ihl95/GuitarWorld/GuitarWorldApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ihl95.GuitarWorld;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
