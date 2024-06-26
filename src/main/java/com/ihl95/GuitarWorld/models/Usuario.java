2859abb3e3b9f5823c5532bba65138d5c9d23652
Ismael <ismael.h.lamela@gmail.com>
2024-06-26 21:24:04 +0200

    fix(Usuario): No args and args constructor implemented

--- src/main/java/com/ihl95/GuitarWorld/models/Usuario.java
+++ src/main/java/com/ihl95/GuitarWorld/models/Usuario.java
@@ -9,12 +9,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
   
   @Id 
